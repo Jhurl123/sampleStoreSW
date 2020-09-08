457fb3682c92f28948b84e40bae304d36ca1c0a0
jhurl123 <justin@Justins-MBP.attlocal.net>
2020-09-07 20:13:47 -0400

    Addition of Mocha and Chai to project. Addition of new test file

---
+++ src/server/test/test.js
@@ -0,0 +1,14 @@
+let chai = require('chai');
+let chaiHttp = require('chai-http');
+let server = require('../server');
+let should = chai.should();
+
+chai.use(chaiHttp);
+
+describe('Product API', () => {
+  it('GET featured products', (done) => {
+    // chai.request(server)
+    // .get
+    // API testing here
+  })
+})
